7dd0ecf2b97601f4beee9e536344222aec6453fa
Shanvithareddy <shanvithareddy.vadde@gmail.com>
2025-07-05 00:40:00 +0530

    Time: 0 ms (100%), Space: 25.8 MB (35.59%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -11,31 +11,49 @@
  */
 class Solution {
 public:
-    int INF = pow(10,5) + 1;
-    int get_min(TreeNode * root){
-        if(root->left == nullptr) return root->val;
-        else return get_min(root->left);
+    // int INF = pow(10,5) + 1;
+    // int get_min(TreeNode * root){
+    //     if(root->left == nullptr) return root->val;
+    //     else return get_min(root->left);
+    // }
+    // int get_max(TreeNode * root){
+    //     if(root ->right == nullptr) return root->val;
+    //     else return get_max(root->right);
+    // }
+    void get_els(vector<int> & nums, TreeNode * root){
+        if(root == nullptr) return;
+        get_els(nums,root->left);
+        nums.push_back(root->val);
+        get_els(nums,root->right);
     }
-    int get_max(TreeNode * root){
-        if(root ->right == nullptr) return root->val;
-        else return get_max(root->right);
-    }
+
+
+
+
     int getMinimumDifference(TreeNode* root) {
-        // int minofRight;
-        // int maxofLeft;
-        if(root == nullptr) return INF;
-        int rootval = root->val;
-        int ans1 = INF;
-        int ans2 = INF;
-        if(root->left != nullptr){
-            ans1 = rootval - get_max(root->left);
-            // if(root-> right == nullptr) return ans1;
-        }
-        if(root->right != nullptr){
-            ans2 = get_min(root->right) - rootval;
-        }
+        // // int minofRight;
+        // // int maxofLeft;
+        // if(root == nullptr) return INF;
+        // int rootval = root->val;
+        // int ans1 = INF;
+        // int ans2 = INF;
+        // if(root->left != nullptr){
+        //     ans1 = rootval - get_max(root->left);
+        //     // if(root-> right == nullptr) return ans1;
+        // }
+        // if(root->right != nullptr){
+        //     ans2 = get_min(root->right) - rootval;
+        // }
         
-        return min(min(ans1,ans2),min(getMinimumDifference(root->left),getMinimumDifference(root->right)));
+        // return min(min(ans1,ans2),min(getMinimumDifference(root->left),getMinimumDifference(root->right)));
+        vector<int> nums;
+        get_els(nums, root);
+        int diff = nums[1] - nums[0];
+        for(int i=2;i<nums.size();i++){
+            int temp = nums[i] - nums[i-1];
+            if(temp < diff) diff = temp;
+        }
+        return diff;
         
     }
 };
