de8283a734cc4ae522559e41bc80dda042030556
Shanvithareddy <shanvithareddy.vadde@gmail.com>
2025-07-09 01:14:33 +0530

    Time: 0 ms (100%), Space: 9 MB (6.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // if(x == 0 || x == 1) return x;
-        // if(n >=0){
-        //     double pro = 1;
-        //     for(double i =0;i<n;i++){
-        //         pro = pro * x;
-        //     }
-        //     return pro;
-        // }
-        // else{
-        //     double pro = 1;
-        //     for(double i =0;i< -1 * n;i++){
-        //         pro = pro / x;
-        //     }
-        //     return pro;
-        // }
         return pow(x,n);
     }
 };
