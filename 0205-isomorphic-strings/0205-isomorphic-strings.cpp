0d3b433130c335f685830ca1e7adef2979c56971
Shanvithareddy <shanvithareddy.vadde@gmail.com>
2025-07-12 23:43:28 +0530

    Time: 0 ms (100%), Space: 9.5 MB (29.62%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         if(s.length()!= t.length()) return false;
-        map<char,char> d;
+        map<char,char> d1;
+        map<char,char> d2;
         for(int i=0;i<s.length();i++){
-            if(d.contains(s[i])){
-                if(d[s[i]] != t[i]) return false;
+            if(d1.contains(s[i])){
+                if(d1[s[i]] != t[i]) return false;
+            }
+            if(d2.contains(t[i])){
+                if(d2[t[i]]!= s[i]) return false;
             }
             else{
-                d[s[i]] = t[i];
+                d1[s[i]] = t[i];
+                d2[t[i]] = s[i];
             }
         }
-        map<char,char> m;
-        for(int i=0;i<t.length();i++){
-            if(m.contains(t[i])){
-                if(m[t[i]]!= s[i]) return false;
-            }
-            else{
-                m[t[i]] = s[i];
-            }
-        }
+        
         return true;
     }
 };
