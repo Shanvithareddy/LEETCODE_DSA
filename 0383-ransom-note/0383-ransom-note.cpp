0bca8f86ad41111f00882e905f8c17f1d52a2cca
Shanvithareddy <shanvithareddy.vadde@gmail.com>
2025-07-12 23:16:39 +0530

    Time: 4 ms (48.95%), Space: 11.8 MB (52.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,19 +3,21 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> counts;
         for(int i=0;i<magazine.length();i++){
-            if(counts.contains(magazine[i])){
-                counts[magazine[i]]++;
-            }
-            else{
-                counts[magazine[i]] = 1;
-            }
+            // if(counts.contains(magazine[i])){
+            //     counts[magazine[i]]++;
+            // }
+            // else{
+            //     counts[magazine[i]] = 1;
+            // }
+            counts[magazine[i]]++;
+
         }
         for(int i=0;i<ransomNote.length();i++){
-            if(counts.contains(ransomNote[i])){
+            // if(counts.contains(ransomNote[i])){
                 counts[ransomNote[i]]--;
                 if(counts[ransomNote[i]]<0) return false;
-            }
-            else return false;
+            // }
+            // else return false;
         }
         return true;
     }
